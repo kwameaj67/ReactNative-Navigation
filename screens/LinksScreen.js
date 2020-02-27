c66749d3ed930a9202356046799d8670e04c5b4a
unknown <kwameagyenimboateng64@gmail.com>
2020-02-27 01:30:34 +0000

    initial commit

---
+++ screens/LinksScreen.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function LinksScreen() {
+  return (
+   <View style={styles.container}>
+
+   </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+});
