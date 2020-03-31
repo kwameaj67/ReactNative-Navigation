61b3a83a5632900de50410db91992cad9a15a9bf
unknown <kwameagyenimboateng64@gmail.com>
2020-03-31 19:29:31 +0000

    added alert to profile

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,6 +1,5 @@
 import  React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View,Button } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import {StyleSheet,View,Button } from 'react-native';
 import { AuthContext } from '../Auth/AuthContext'
 
 export default function Profile({route,navigation}) {
@@ -8,7 +7,7 @@ export default function Profile({route,navigation}) {
   return (
     <View style={styles.container}>
          <View style={{alignItems:'center'}}>
-              <Button title="click me" onPress={()=>{navigation.goBack()}}/>
+              <Button title="click me" onPress={()=>{alert("Profile screen")}}/>
               <Button title="open drawer" onPress={()=>{navigation.toggleDrawer()}}/>
               <Button title="signout" onPress={()=>{signOut()}}/>
          </View>
